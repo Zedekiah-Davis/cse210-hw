2baf21e7e22028fa747482db4a4776e3668eb34b
Zedekiah-Davis <131714646+Zedekiah-Davis@users.noreply.github.com>
2024-01-17 12:33:39 -0700

    prep 2 progress

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            string letter = ("A");
+        }
+        else if (grade >= 80)
+        {
+            letter = ("B");
+        }
+        else if (grade >= 70)
+        {
+            
+        }
+        else if (grade >= 60)
+        {
+            
+        }
+        else
+        {
+            
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass.");
+        }
     }
 }
