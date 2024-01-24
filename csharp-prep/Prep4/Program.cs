4563c70fa31e233d8e6174267f1cfba65102638f
Zedekiah-Davis <131714646+Zedekiah-Davis@users.noreply.github.com>
2024-01-24 12:33:59 -0700

    prep 4 done, prep 5 progress

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int active = 1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        do
+        {
+            Console.Write("Enter Number: ");
+            int completeTest = int.Parse(Console.ReadLine());
+            
+            if (completeTest == 0)
+            {
+                Console.WriteLine($"The sum is: {numbers.Sum()}");
+                Console.WriteLine($"The average is: {numbers.Average()}");
+                Console.WriteLine($"The largest number is: {numbers.Max()}");
+                active = 0;
+            }
+            else
+            {
+                numbers.Add(completeTest);
+            }
+        } while (active == 1);
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
+    static void DisplayWelcome()
+        {
+            Console.Write("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int SquareNumber(int favorite_Number)
+        {
+           int squaredNumber = favorite_Number * favorite_Number;
+           return squaredNumber;
+        }
 }
