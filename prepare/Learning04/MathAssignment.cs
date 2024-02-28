5cada003c641b852d7d2ae3ded8b54c2f4752547
Zedekiah-Davis <131714646+Zedekiah-Davis@users.noreply.github.com>
2024-02-28 12:38:23 -0700

    learning 4 update

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -4,11 +4,12 @@ public class MathAssignment : Assignment
     private string _problems;
 
     public MathAssignment(string studentName, string topic, string textbookSection, string problems)
-        : base(studentName,topic);
+        : base(studentName, topic);
     {
         _textbookSection = textbookSection;
         _problems = problems;
     }
+    
     public string GetHomeworkList()
     {
         return $"Section {_textbookSection} Problems {_problems}";
