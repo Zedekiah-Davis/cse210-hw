33371e919f413e06fac490af7e3a25772e30392d
Zedekiah-Davis <zedekiahdavis1@gmail.com>
2024-02-28 17:09:22 -0700

    learning 4 done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,6 +7,12 @@ class Program
         Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(a1.GetSummary());
 
-        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
